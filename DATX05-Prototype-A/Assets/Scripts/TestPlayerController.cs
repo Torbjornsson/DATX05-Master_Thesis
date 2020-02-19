0db7c0ee981fd55a3ab9d340358527570c150c0d
Zephra <robin.lilius@gmail.com>
2020-02-19 15:45:26 +0100

    adds xr sdk management

--- DATX05-Prototype-A/Assets/Scripts/TestPlayerController.cs
+++ DATX05-Prototype-A/Assets/Scripts/TestPlayerController.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerController : MonoBehaviour
+public class TestPlayerController : MonoBehaviour
 {
     public int speed = 0;
 
