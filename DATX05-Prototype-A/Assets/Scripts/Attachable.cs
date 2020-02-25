20a25eca0216cf42cc16b1e718bf2f55e6ac288a
bob <robin.lilius@gmail.com>
2020-02-25 08:19:05 +0100

    Applied try to fix rigidbody-conflicts

--- DATX05-Prototype-A/Assets/Scripts/Attachable.cs
+++ DATX05-Prototype-A/Assets/Scripts/Attachable.cs
@@ -4,11 +4,12 @@ using UnityEngine;
 
 public class Attachable : MonoBehaviour
 {
-    public BoxCollider myAttachZone;
-    public BoxCollider targetAttachZone;
-    public GameObject originalParent;
+    public Collider myCollider;
+    // public BoxCollider myAttachZone;
+    // public BoxCollider targetAttachZone;
     public Rigidbody rb;
 
+    private GameObject originalParent;
     private GameObject overlappingTarget = null;
 
     private GameObject attachedTo = null;
@@ -17,50 +18,31 @@ public class Attachable : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
-    }
+        if (!myCollider) Debug.LogError("Couldn't find my collider!");
 
-    // Update is called once per frame
-    void Update()
-    {
-        // overlappingTarget = false;
-        // Debug.Log("Resetting overlapping target...");
-    }
-
-    void FixedUpdate() {
-        // if (attachedTo != null) {
-        //     transform.position = attachedTo.transform.position + attachedOffset;
-        //     transform.rotation = attachedTo.transform.rotation;
-        //     Debug.Log("Attached to: "+attachedTo+", position: "+attachedTo.transform.position + attachedOffset);
-        // }
+        originalParent = transform.parent.gameObject;
+        if (!originalParent) Debug.LogError("Couldn't find original parent!");
     }
 
     void OnTriggerEnter(Collider other) {
         if (other.tag.Equals("TargetZone")) {
             overlappingTarget = other.gameObject;
-            // Debug.Log("Overlapping target!");
         }
     }
 
     void OnTriggerExit(Collider other) {
         if (other.tag.Equals("TargetZone")) {
             overlappingTarget = null;
-            // Debug.Log("Overlapping target!");
+        }
+    }
+
+    public void CatchGrabStartEvent(OVRGrabbable grabbable) {
+        if (attachedTo != null) {
+            DetachFromObject();
         }
     }
     
-    // private Collider[] results;
-    // public Collider[] result = new Collider[1];
     public void CatchGrabEndEvent(OVRGrabbable grabbable) {
-        // Debug.Log("Dropping grabbable! (overlapping? "+overlappingTarget+")");
-
-        // Collider[] results = new Collider[1];
-        // var pos = myAttachZone.transform.position + myAttachZone.gameObject.transform.position;
-        // var size = myAttachZone.bounds.size / 2f;
-        // Debug.Log("Pos: "+pos+", size: "+size);
-        // var hits = Physics.OverlapBoxNonAlloc(pos, size, results, myAttachZone.transform.rotation);
-
-        // if (hits > 0 && results[0].tag.Equals("TargetZone")) {
         if (overlappingTarget && attachedTo == null) {
             AttachToObject(overlappingTarget);
         }
@@ -72,17 +54,18 @@ public class Attachable : MonoBehaviour
         transform.position = go.transform.position;
         transform.rotation = go.transform.rotation;
 
-        rb.isKinematic = true;
+        // rb.isKinematic = true;
+        myCollider.enabled = false;
+            
         attachedTo = go;
         gameObject.transform.SetParent(go.transform);
-        // attachedOffset = transform.position - go.transform.position;
     }
 
-    public void CatchGrabStartEvent(OVRGrabbable grabbable) {
-        if (attachedTo != null) {
-            gameObject.transform.SetParent(originalParent.transform);
-            attachedTo = null;
-            attachedOffset = Vector3.zero;
-        }
+    public void DetachFromObject()
+    {
+        gameObject.transform.SetParent(originalParent.transform);
+        attachedTo = null;
+        attachedOffset = Vector3.zero;
+        myCollider.enabled = true;
     }
 }
