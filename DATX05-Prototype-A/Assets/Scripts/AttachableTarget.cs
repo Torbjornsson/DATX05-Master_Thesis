f487f47d446982dcf6d45d450dc61b3e3cddb429
bob <robin.lilius@gmail.com>
2020-02-26 11:43:44 +0100

    Fixed attachable tiles

---
+++ DATX05-Prototype-A/Assets/Scripts/AttachableTarget.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AttachableTarget : MonoBehaviour
+{
+    public BoxCollider myMainCollider;
+
+    private Vector3 unAttachedColliderCenter;
+    private Vector3 unAttachedColliderSize;
+    public Vector3 attachedColliderCenter;
+    public Vector3 attachedColliderSize;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        unAttachedColliderSize = myMainCollider.size;
+        unAttachedColliderCenter = myMainCollider.center;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void AttachObject() {
+        myMainCollider.size = attachedColliderSize;
+        myMainCollider.center = attachedColliderCenter;
+    }
+
+    public void DetachObject() {
+        myMainCollider.size = unAttachedColliderSize;
+        myMainCollider.center = unAttachedColliderCenter;
+    }
+}
