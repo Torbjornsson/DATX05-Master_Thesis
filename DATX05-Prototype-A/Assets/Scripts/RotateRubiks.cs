8206a06b78f488107c1f143daa2cb870300869e0
Torbjornsson <david.torbjornsson@gmail.com>
2020-02-28 14:24:10 +0100

    fixes rubiks kub rotations

--- DATX05-Prototype-A/Assets/Scripts/RotateRubiks.cs
+++ DATX05-Prototype-A/Assets/Scripts/RotateRubiks.cs
@@ -32,29 +32,32 @@ public class RotateRubiks : MonoBehaviour
                 case "RightHand":
                     if (OVRInput.Get(OVRInput.Axis1D.PrimaryIndexTrigger) > 0.7f && !isRotationStarted)
                     {
-                        int direction = 1;
+                        int direction = 0;
                         Vector3 axis = PickAxis("LeftHand", out direction);
-                        RotateFace(axis, direction);
-                        //RotateFront();
+                        if (axis != Vector3.zero && direction != 0 && frontFace.transform.childCount == 4)
+                            RotateFace(axis, direction);
                     }
 
                     if (OVRInput.Get(OVRInput.Axis1D.PrimaryIndexTrigger) <= 0.5f && isRotationStarted)
-                        StopRotation(frontFace);
+                        StopRotation();
                     break;
 
                 case "LeftHand":
                     if (OVRInput.Get(OVRInput.Axis1D.SecondaryIndexTrigger) > 0.7f && !isRotationStarted)
                     {
-                        RotateFront();
+                        int direction = 0;
+                        Vector3 axis = PickAxis("RightHand", out direction);
+                        if (axis != Vector3.zero && direction != 0 && frontFace.transform.childCount == 4)
+                            RotateFace(axis, direction);
                     }
 
                     if (OVRInput.Get(OVRInput.Axis1D.SecondaryIndexTrigger) <= 0.5f && isRotationStarted)
-                        StopRotation(frontFace);
+                        StopRotation();
                     break;
 
                 default:
                     if(isRotationStarted)
-                        StopRotation(frontFace);
+                        StopRotation();
                     break;
             }
         }
@@ -69,45 +72,47 @@ public class RotateRubiks : MonoBehaviour
         {
             if (axis.x > 0)
             {
-                if (smallCube.intPos.x == side)
-                {
-                    smallCube.transform.SetParent(smallCube.intPos.x == side ? frontFace.transform : backFace.transform);
-                    Debug.Log(smallCube.transform.parent.name);
-                }
+                smallCube.transform.SetParent(smallCube.intPos.x == side ? frontFace.transform : backFace.transform);
             }
+
             else if (axis.y > 0)
             {
-                if (smallCube.intPos.y == side)
-                {
-                    smallCube.transform.SetParent(smallCube.intPos.y == side ? frontFace.transform : backFace.transform);
-                    Debug.Log(smallCube.transform.parent.name);
-                }
+                smallCube.transform.SetParent(smallCube.intPos.y == side ? frontFace.transform : backFace.transform);
             }
+
             else if (axis.z > 0)
             {
                 smallCube.transform.SetParent(smallCube.intPos.z == side ? frontFace.transform : backFace.transform);
-                Debug.Log(smallCube.transform.parent.name);
             }
         }
+        Debug.Log("Rotates cubes: " + frontFace.transform.childCount);
     }
 
-    void StopRotation(GameObject f)
+    void StopRotation()
     {
-        Quaternion rot = f.transform.localRotation;
-        rot.eulerAngles = rot.eulerAngles/90f;
-        Vector3 euler = new Vector3();
-        euler.x = Mathf.Round(rot.eulerAngles.x);
-        euler.y = Mathf.Round(rot.eulerAngles.y);
-        euler.z = Mathf.Round(rot.eulerAngles.z);
-        euler *= 90f;
-        rot.eulerAngles = euler;
+        Vector3Int prevPos;
+        // Quaternion rot = frontFace.transform.localRotation;
+        // rot.eulerAngles = rot.eulerAngles/90f;
+        // Vector3 euler = new Vector3();
+        // euler.x = Mathf.Round(rot.eulerAngles.x);
+        // euler.y = Mathf.Round(rot.eulerAngles.y);
+        // euler.z = Mathf.Round(rot.eulerAngles.z);
+        // euler *= 90f;
+        // rot.eulerAngles = euler;
 
-        f.transform.localRotation = rot;
+        // frontFace.transform.localRotation = rot;
 
         foreach(var smallCube in smallCubes)
         {
             smallCube.transform.SetParent(transform);
+            prevPos = smallCube.intPos;
+            smallCube.UpdatePosition();
+            Debug.Log("from position " + prevPos + " to position " + smallCube.intPos);
         }
+
+        SetFace(frontFace);
+        SetFace(backFace);
+                
         isRotationStarted = false;
         Debug.Log("Rotation Stopped");
     }
@@ -116,60 +121,90 @@ public class RotateRubiks : MonoBehaviour
         direction = 0;
         int side = 0;
         int layerMask = 1 << 12;
-        Vector3 axis = new Vector3();
+        Vector3 axis = Vector3.zero;
         RaycastHit hit;
         GameObject activeHand = GameObject.FindGameObjectWithTag(hand);
         
         if(Physics.Raycast(activeHand.transform.position, activeHand.transform.TransformDirection(Vector3.forward), out hit, 1f, layerMask))
         {
-            GameObject hitCube = hit.collider.gameObject;
-            SmallCube sCube = hitCube.GetComponent<SmallCube>();
-            Debug.Log(hitCube.name);
-            if (hit.collider == sCube.colliderFA)
+            Debug.DrawRay(activeHand.transform.position, activeHand.transform.TransformDirection(Vector3.forward), Color.red, 1);
+            Debug.Log(hit);
+            if (hit.collider)
             {
-                switch(hitCube.name){
-                    case "Cube 0x0x0":
+                
+                switch(hit.collider.tag){
+                    case "FDL":
                         axis = Vector3.right;
                         direction = -1;
                         side = 0;
                         break;
-                    case "Cube 0x1x0":
-                        axis = Vector3.right;
-                        direction = 1;
-                        side = 0;
-                        break;
-                    case "Cube 1x0x0":
-                        axis = Vector3.up;
-                        direction = -1;
-                        side = 0;
-                        break;
-                    case "Cube 1x1x0":
+                    case "FDR":
                         axis = Vector3.right;
                         direction = 1;
                         side = 1;
                         break;
-                    case "Cube 0x0x1":
+                    case "FLD":
+                        axis = Vector3.up;
+                        direction = 1;
+                        side = 0;
+                        break;
+                    case "FLT":
+                        axis = Vector3.up;
+                        direction = 1;
+                        side = 1;
+                        break;
+                    case "FTL":
                         axis = Vector3.right;
                         direction = -1;
                         side = 0;
                         break;
+                    case "FTR":
+                        axis = Vector3.right;
+                        direction = 1;
+                        side = 1;
+                        break;
+                    case "FRD":
+                        axis = Vector3.up;
+                        direction = -1;
+                        side = 0;
+                        break;
+                    case "FRT":
+                        axis = Vector3.up;
+                        direction = -1;
+                        side = 1;
+                        break;
+                    case "DLF":
+                        axis = Vector3.forward;
+                        direction = -1;
+                        side = 0;
+                        break;
+                    case "DLB":
+                        axis = Vector3.forward;
+                        direction = -1;
+                        side = 1;
+                        break;
+                    case "DRF":
+                        axis = Vector3.forward;
+                        direction = 1;
+                        side = 0;
+                        break;
+                    case "DRB":
+                        axis = Vector3.forward;
+                        direction = 1;
+                        side = 1;
+                        break;
                 }
 
-                StartRotation(axis, side);
+                Debug.Log("Collider: " + hit.collider.name);
+                if(axis != Vector3.zero)
+                    StartRotation(axis, side);
             }
         }   
         return axis;
     }
-
-    void RotateFront()
-    {
-        StartRotation(Vector3.forward, 0);
-        frontFace.transform.localEulerAngles = new Vector3(0, 0, 30);
-    }
-
     void RotateFace(Vector3 axis, int direction)
     {
-        frontFace.transform.localEulerAngles = axis * (direction * 90);
+        frontFace.transform.localEulerAngles += axis * (direction * 90);
     }
     void SetFace(GameObject face)
     {
--- DATX05-Prototype-A/Assets/Scripts/SmallCube.cs
+++ DATX05-Prototype-A/Assets/Scripts/SmallCube.cs
@@ -7,19 +7,15 @@ public class SmallCube : MonoBehaviour
     public Vector3Int intPos;
     Vector3 floatPos;
 
-    public BoxCollider colliderFA, colliderFB, colliderDA, colliderDB, colliderLA, colliderLB;
-    public BoxCollider[] colliders;
     // Start is called before the first frame update
     void Start()
     {
-        colliders = GetComponents<BoxCollider>();
         UpdatePosition();
     }
 
     // Update is called once per frame
     void Update()
     {
-        UpdatePosition();
     }
 
     public void RotateFace(Vector3 parentPosition, Vector3 axiz)
@@ -27,10 +23,10 @@ public class SmallCube : MonoBehaviour
         transform.RotateAround(parentPosition, Vector3.forward, 10);
         UpdatePosition();
     }
-    void UpdatePosition()
+    public void UpdatePosition()
     {
         floatPos = transform.localPosition + new Vector3(0.05f, 0.05f, 0.05f);
-        floatPos.Normalize();
+        floatPos *= 10;        
 
         intPos.x = Mathf.RoundToInt(floatPos.x);
         intPos.y = Mathf.RoundToInt(floatPos.y);
