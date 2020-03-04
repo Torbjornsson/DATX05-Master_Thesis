4e22a04d6c6881f06b4002399fac691c65f92ede
Bob <robin.lilius@gmail.com>
2020-03-04 10:17:44 +0100

    Created new test-scripts and -cubes, to try out a slightly different solution

--- DATX05-Prototype-A/Assets/Scripts/RotateRubiks_Test.cs
+++ DATX05-Prototype-A/Assets/Scripts/RotateRubiks_Test.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class RotateRubiks : MonoBehaviour
+public class RotateRubiks_Test : MonoBehaviour
 {
     public AudioClip[] shuffleSounds;
     public AudioSource soundSource;
