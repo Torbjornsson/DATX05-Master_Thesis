c498fdab4abeca120a02f17265c46bb8b0216a66
Bob <robin.lilius@gmail.com>
2020-03-05 16:28:01 +0100

    OnBoarding!

---
+++ DATX05-Prototype-A/Assets/VaseConfetti.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VaseConfetti : MonoBehaviour
+{
+    public ParticleSystem confetti;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter(Collider other) {
+        if (other.tag.Equals("OnBoardCube")) {
+            confetti.Play();
+        }
+    }
+}
