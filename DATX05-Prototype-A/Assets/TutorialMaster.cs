8297ba26a9e365871afec62c72b87bdf74e3dffa
Bob <robin.lilius@gmail.com>
2020-03-11 11:46:44 +0100

    Created a tutorial master component

---
+++ DATX05-Prototype-A/Assets/TutorialMaster.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class TutorialMaster : MonoBehaviour
+{
+    [Range(0, 10)] public int maxStates = 0;
+    public TutorialStateEvent triggerSlideTransitionEvent;
+    public TutorialStateEvent setTutorialState;
+
+    [System.Serializable]
+    public class TutorialStateEvent : UnityEvent<int> { }
+
+    public int tutorialState {get; private set;}
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (maxStates <= 0)
+            Debug.LogWarning("TutorialMaster: WARNING - Max States is not set to a positive integer!");
+
+        if (triggerSlideTransitionEvent == null)
+            triggerSlideTransitionEvent = new TutorialStateEvent();
+        if (setTutorialState == null)
+            setTutorialState = new TutorialStateEvent();
+
+        tutorialState = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void TriggerNextSlide() {
+        triggerSlideTransitionEvent.Invoke(tutorialState + 1);
+    }
+
+    public void IncrementTutorialState() {
+        tutorialState++;
+    }
+
+    public void ArrivedAtSlide(int state) {
+        tutorialState = state;
+    }
+
+    public void SeTutorialState(int state) {
+        tutorialState = state;
+        setTutorialState.Invoke(state);
+    }
+}
