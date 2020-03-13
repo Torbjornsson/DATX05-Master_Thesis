de90b87d8fcd129e4b2eee248182b698c9c98835
Bob <robin.lilius@gmail.com>
2020-03-13 16:02:39 +0100

    Added all tutorial texts to textfiles and dynamically populate the tutorial slides with it

--- DATX05-Prototype-A/Assets/Scripts/ITutorial.cs
+++ DATX05-Prototype-A/Assets/Scripts/ITutorial.cs
@@ -1,11 +1,26 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public abstract class ITutorial : MonoBehaviour
 {
-    public GameObject[] tutorialSlides;
+    [Header("ITutorial Parameters")]
+    [Range(1,3)] public int tutorialForPuzzle = 1;
+    public bool useOnBoarding = false;
+    [Space]
+    public GameObject[] onBoardingSlides;
+    public GameObject[] puzzle1Slides;
+    public GameObject[] puzzle2Slides;
+    public GameObject[] puzzle3Slides;
     public GameObject transitionSlide;
+    [Space]
+    public TextAsset onBoardingTexts;
+    public TextAsset puzzle1Texts;
+    public TextAsset puzzle2Texts;
+    public TextAsset puzzle3Texts;
+
+    protected List<GameObject> activeSlides;
 
     protected TutorialMaster master;
 
@@ -15,16 +30,51 @@ public abstract class ITutorial : MonoBehaviour
     // Start is called before the first frame update
     public virtual void Start()
     {
-        if (tutorialSlides.Length <= 0)
-            Debug.LogError("ITutorial: Tutorial does not have a single slide!");
+        // Checking that all slide arrays are there
+        if (onBoardingSlides.Length <= 0)
+            Debug.LogError("ITutorial: Tutorial does not have a single On-Boading-slide!");
+        if (puzzle1Slides.Length <= 0)
+            Debug.LogError("ITutorial: Tutorial does not have a single Puzzle 1 slide!");
+        if (puzzle2Slides.Length <= 0)
+            Debug.LogError("ITutorial: Tutorial does not have a single Puzzle 2 slide!");
+        if (puzzle3Slides.Length <= 0)
+            Debug.LogError("ITutorial: Tutorial does not have a single Puzzle 3 slide!");
         
-        foreach (GameObject go in tutorialSlides) {
+        // Checking each item in every slide array
+        foreach (GameObject go in onBoardingSlides) {
             if (!go)
-                Debug.LogError("ITutorial: One of the given tutorial Game Objects does not exist!");
+                Debug.LogError("ITutorial: One of the given On-Boarding slide Game Objects does not exist!");
         }
+        foreach (GameObject go in puzzle1Slides) {
+            if (!go)
+                Debug.LogError("ITutorial: One of the given Puzzle 1 slide Game Objects does not exist!");
+        }
+        foreach (GameObject go in puzzle2Slides) {
+            if (!go)
+                Debug.LogError("ITutorial: One of the given Puzzle 2 slide Game Objects does not exist!");
+        }
+        foreach (GameObject go in puzzle3Slides) {
+            if (!go)
+                Debug.LogError("ITutorial: One of the given Puzzle 3 slide Game Objects does not exist!");
+        }
+        
+        // Checking for tutorial texts
+        if (!onBoardingTexts)
+            Debug.LogError("ITutorial: On-Boarding tutorial texts not found!");
+        if (!puzzle1Texts)
+            Debug.LogError("ITutorial: Puzzle 1 tutorial texts not found!");
+        if (!puzzle2Texts)
+            Debug.LogError("ITutorial: Puzzle 2 tutorial texts not found!");
+        if (!puzzle3Texts)
+            Debug.LogError("ITutorial: Puzzle 3 tutorial texts not found!");
 
+        activeSlides = CompileTutorialSlides();
+
+        // Letting tutorial master know how many total slides there are
         master = GameMaster.instance.tutorialMaster;
-        master.SetMaxStates(tutorialSlides.Length - 1);
+        master.SetMaxStates(activeSlides.Count - 1);
+
+        DistributeTexts();
     }
 
     public virtual void TriggerNextSlide(int nextState) {
@@ -34,5 +84,60 @@ public abstract class ITutorial : MonoBehaviour
     public void ArrivedAtSlide(int state) {
         currentState = state;
         master.ArrivedAtSlide(state);
+    }
+
+    // Compiling the active slides for this specific tutorial
+    protected List<GameObject> CompileTutorialSlides() {
+        var list = new List<GameObject>();
+        if (useOnBoarding)
+            list.AddRange(onBoardingSlides);
+        list.AddRange(GetSlides(tutorialForPuzzle));
+        return list;
+    }
+
+    protected void DistributeTexts() {
+        if (useOnBoarding)
+            DistributeTexts(onBoardingTexts, onBoardingSlides);
+        DistributeTexts(GetTexts(tutorialForPuzzle), GetSlides(tutorialForPuzzle));
+    }
+    
+    protected void DistributeTexts(TextAsset texts, GameObject[] slides) {
+        var textsSeparated = GetTextsFromFile(texts);
+        for(int i = 0; i < slides.Length; i++) {
+            if (textsSeparated.Length > i) {
+                var text = textsSeparated[i].Trim(' ', '\n', '\r');
+                DistributeTextToSlide(text, slides[i]);
+            }
+        }
+    }
+
+    protected abstract void DistributeTextToSlide(string text, GameObject slide);
+
+    protected string[] GetTextsFromFile(TextAsset textFile) {
+        // var splitters = new string[1];
+        // // splitters[0] = ";\n";
+        // splitters[0] = ";";
+        // var strings = textFile.text.Split(splitters, StringSplitOptions.RemoveEmptyEntries);
+        // Debug.Log("Just split a string into "+strings.Length+" parts. First entry: "+strings[0]);
+        // return strings;
+        return textFile.text.Split(';');
+    }
+
+    protected GameObject[] GetSlides(int puzzleNumber) {
+        switch(tutorialForPuzzle) {
+            case 1: return puzzle1Slides;
+            case 2: return puzzle2Slides;
+            case 3: return puzzle3Slides;
+        }
+        throw new ArgumentException("ITutorial.GetSlides() : Puzzle number can only be 1, 2 or 3!");
+    }
+
+    protected TextAsset GetTexts(int puzzleNumber) {
+        switch(tutorialForPuzzle) {
+            case 1: return puzzle1Texts;
+            case 2: return puzzle2Texts;
+            case 3: return puzzle3Texts;
+        }
+        throw new ArgumentException("ITutorial.GetSlides() : Puzzle number can only be 1, 2 or 3!");
     }
 }
