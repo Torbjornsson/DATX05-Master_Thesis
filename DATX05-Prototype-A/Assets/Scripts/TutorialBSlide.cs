374298fdb2d02c87208a7f8faf93ad1b10400b23
Bob <robin.lilius@gmail.com>
2020-03-16 16:32:32 +0100

    All slides work

--- DATX05-Prototype-A/Assets/Scripts/TutorialBSlide.cs
+++ DATX05-Prototype-A/Assets/Scripts/TutorialBSlide.cs
@@ -7,7 +7,6 @@ public class TutorialBSlide : MonoBehaviour
     public GameObject[] slideContents;
     public SimpleHelvetica textScript;
     public int lineSpacing = 30;
-    // public string startingText = "[No text input]";
     public bool dynamicPosition = true;
 
     public Vector3 startingPosition;
@@ -22,16 +21,9 @@ public class TutorialBSlide : MonoBehaviour
         startingRotation = transform.localEulerAngles;
 
         textScript.LineSpacing = lineSpacing;
-        // SetText("Testing text with several new rows:\nBla bla bla\n\nBla bla bla\n\nBla bla bla");
-        // UpdateTextPosition();
-        // if (!startingText.Equals(""))
-        //     SetText(startingText);
-        // else
+        
         if (dynamicPosition)
             UpdateTextPosition();
-
-        Debug.Log("Setting start text");
-        // textScript.
         
         gameObject.SetActive(false);
     }
@@ -49,7 +41,6 @@ public class TutorialBSlide : MonoBehaviour
     }
 
     public void UpdateTextPosition() {
-        // Debug.Log("Text height: "+textScript.height);
         var trans = textScript.gameObject.transform;
         var localPos = trans.localPosition;
         localPos.y = textScript.height / 2 - (textScript.LineSpacing / 2) * textScript.transform.localScale.y;
@@ -62,7 +53,7 @@ public class TutorialBSlide : MonoBehaviour
         transform.localPosition = pos;
 
         var rot = startingRotation;
-        rot.x += progress * transitionRotation;
+        rot.x -= progress * transitionRotation;
         rot.y += progress * transitionRotation * 0.1f;
         transform.localEulerAngles = rot;
     }
