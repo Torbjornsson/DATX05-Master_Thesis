3e082838c8162806eff3b079e4fa62784f94beb1
torbjornson <david.torbjornsson@gmail.com>
2020-04-03 10:36:16 +0200

    fixing tile scrambling

--- DATX05-Prototype-A/Assets/Scripts/TilesSetup.cs
+++ DATX05-Prototype-A/Assets/Scripts/TilesSetup.cs
@@ -4,17 +4,14 @@ using UnityEngine;
 
 public class TilesSetup : MonoBehaviour
 {
-    private GameObject[] tiles;
+    public GameObject[] tiles;
 
     private List<Vector3> positions = new List<Vector3>();
+
+    private Vector3 originalPosition;
     // Start is called before the first frame update
     void Start()
     {
-        tiles = GameObject.FindGameObjectsWithTag("Tile");
-
-        ScrambleTiles(tiles);
-
-        ScrambleTiles(tiles);
     }
 
     private Vector3 GetRandomPosition(List<Vector3> positions)
@@ -30,6 +27,7 @@ public class TilesSetup : MonoBehaviour
 
     private void ScrambleTiles(GameObject[] tiles)
     {
+        Debug.Log("scramble tiles");
         for (int i = 0; i < tiles.Length; i++)
         {
             positions.Add(tiles[i].transform.position);
@@ -44,6 +42,24 @@ public class TilesSetup : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if (tiles == null || tiles.Length == 0)
+        {
+            tiles = GameObject.FindGameObjectsWithTag("Tile");
+        }
+        if (tiles != null || tiles.Length > 0)
+        {
+            if (originalPosition == Vector3.zero)
+            {
+                originalPosition = tiles[4].transform.position;
 
+                ScrambleTiles(tiles);
+
+                ScrambleTiles(tiles);
+            }
+            else if (tiles[4].transform.position == originalPosition)
+            {
+                ScrambleTiles(tiles);
+            }
+        }
     }
 }
